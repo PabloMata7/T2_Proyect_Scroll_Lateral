78ed566294563640fef570412375b435aa2c0725
JorgeDiazEsteban <zarluca@hotmail.com>
2026-01-07 17:27:20 +0100

    tutorial

---
+++ Assets/Scripts/Tutorial move.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Tutorialmove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject text;
+    public float time = 2.5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (text != null)
+            {
+                text.SetActive(true);
+                Invoke("OcultarTexto",time);
+            }
+        }
+    }
+    void OcultarTexto()
+    {
+        text.SetActive(false);
+        Destroy(gameObject);
+    }
+}
