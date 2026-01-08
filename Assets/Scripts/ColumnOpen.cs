02ba109e2734465d2656f065c7875b3d7e1d9d7b
JorgeDiazEsteban <zarluca@hotmail.com>
2026-01-08 12:23:53 +0100

    cambios columna caida

--- Assets/Scripts/ColumnOpen.cs
+++ Assets/Scripts/ColumnOpen.cs
@@ -7,15 +7,23 @@ public class ColumnOpen : MonoBehaviour
     // Start is called before the first frame update
     public GameObject DoorOpen;
     public float ColumnLife = 2.0f;
+
+    public Color colorBrillo = Color.yellow;
+    public float velocidad = 2f;
+    public float intensidadMax = 3f;
+
+    private Material material;
     void Start()
     {
-        
+        material = GetComponent<Renderer>().material;
+        material.EnableKeyword("_EMISSION");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float intensidad = Mathf.PingPong(Time.time * velocidad, intensidadMax);
+        material.SetColor("_EmissionColor", colorBrillo * intensidad);
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
