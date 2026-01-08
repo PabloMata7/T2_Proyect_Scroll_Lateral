112c36c6e5e06be44ab7178c0bb7b4fcaaeeb7c7
JorgeDiazEsteban <zarluca@hotmail.com>
2026-01-08 09:42:51 +0100

    pinchos con daño

---
+++ Assets/Scripts/SpikesDamage.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikesDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Player.Instance.TakePlayerDamage(1.0f);
+            Destroy(gameObject);
+        }
+    }
+}
